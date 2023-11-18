4e5540c9bc7a4e55abde82c1ee3e2706c64e1b7c
Orliluq <orlibet@gmail.com>
2023-11-18 00:07:58 -0400

    dependencias para iniciar

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// import vitest from 'vitest'; // Importa Vitest
+
+/// <reference types="vitest" /> // Agrega la referencia de tipos de Vitest
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    // vitest({
+      // Configuración de Vitest, si es necesario
+    // }),
+  ],
+});
