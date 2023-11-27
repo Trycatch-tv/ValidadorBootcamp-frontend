e1148a46020d02c3b8551639ebf3bf9fce84266f
daniel-villada <cdvillada6@misena.edu.co>
2023-11-27 11:48:50 -0500

    feat: add typescript analyzer

--- src/features/app/App.view.tsx
+++ src/features/app/App.view.tsx
@@ -1,10 +1,16 @@
 import { FC } from "react"
 
 interface Props {
-  currentYear: number
+  currentYear: number,
+  actionName?: String
 }
-const AppView: FC<Props> = ({ currentYear }) => {
-  return <h1>Validador de Bootcamps {currentYear}</h1>
+const AppView: FC<Props> = ({ currentYear, actionName }) => {
+  return (
+    <>
+      <h1>Validador de Bootcamps {currentYear}</h1>
+      <h2>Testeando el github action: {actionName = "Typescript Analizyer"}</h2>
+    </>
+  )
 }
 
 export default AppView
