fb4794dab2583d72a93314ce8400b4c6443ae03c
daniel-villada <cdvillada6@misena.edu.co>
2023-11-29 17:42:12 -0500

    chore: card bootcamp - The respective types of a bootcamp are added, in order not to use any

--- src/features/bootcamps/card/card.bootcamp.view.tsx
+++ src/features/bootcamps/card/card.bootcamp.view.tsx
@@ -1,26 +1,32 @@
 import { Button, Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui'
 import { Star } from 'lucide-react'
-import {FC} from 'react'
+import { FC } from 'react'
 import { Link } from 'react-router-dom'
 
-interface Props {
-    bootcamp:any
+type BootcampTypes = {
+    id: number,
+    name: string,
+    description: string,
+
 }
-const CardBootcampView:FC<Props> = ({bootcamp}) => {
+interface Props {
+    bootcamp: BootcampTypes
+}
+const CardBootcampView: FC<Props> = ({ bootcamp }) => {
     return (
-        <Card  className=" mb-4 max-w-[300px]">
+        <Card className=" mb-4 max-w-[300px]">
             <CardHeader className='flex-row justify-between'>
                 <CardTitle className="text-lg">
                     {bootcamp.name}
                 </CardTitle>
                 <div className="flex">
                     {[1, 2, 3, 4, 5].map(() => (
-                        <Star className='h-4 w-4 text-gray-500'/>
+                        <Star className='h-4 w-4 text-gray-500' />
                     ))}
                 </div>
             </CardHeader>
             <CardContent className="card-body">
-               <p className='whitespace-break-spaces text-gray-400 text-xs'>{bootcamp.description}</p>
+                <p className='whitespace-break-spaces text-gray-400 text-xs'>{bootcamp.description}</p>
             </CardContent>
             <CardFooter className="card-footer">
                 <Link to={`/bootcamp/${bootcamp.id}`}>
