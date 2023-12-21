e9164b1e9c5ae1ac20577d222583db7859f4343c
orliluq <orlibet@gmail.com>
2023-12-21 19:09:15 -0400

    trabajando en la dashboard

---
+++ src/features/bootcamps/admin/admin.bootcamps.view.container.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AdminBootcampsContainer from './admin.bootcamps.container';
+
+const AdminBootcampsViewContainer = () => {
+  return (
+    <div>
+      <AdminBootcampsContainer />
+    </div>
+  );
+};
+
+export default AdminBootcampsViewContainer;
