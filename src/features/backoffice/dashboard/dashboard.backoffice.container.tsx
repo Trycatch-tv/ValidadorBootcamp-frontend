890bf67c72fdca8f31295c679e858a196c92444d
orliluq <orlibet@gmail.com>
2024-01-30 12:46:38 -0400

    cambios y eliminando rutas innecesarias

--- src/features/backoffice/dashboard/dashboard.backoffice.container.tsx
+++ src/features/backoffice/dashboard/dashboard.backoffice.container.tsx
@@ -1,30 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import BackofficeView from "./dashboard.backoffice.view"
 
-interface BackofficeLinkProps {
-    name: string
-    path: string
-  }
-
-const routes: BackofficeLinkProps[] = [
-    {
-      name: "Backoffice de bootcamps",
-      path: "/backoffice/bootcamp",
-    },
-    {
-      name: "Backoffice Reports",
-      path: "/backoffice/reports",
-    },
-    {
-      name: "Backoffice Ranking",
-      path: "/backoffice/ranking",
-    },
-    {
-        name: "Backoffice User",
-        path: "/backoffice/users",
-      },
-  ]
-
 
 const DashboardBackofficeContainer = () => {
   return (
--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import HomeContainer from "./features/home/Home.container.tsx"
 import RankingContainer from "./features/ranking/Ranking.container.tsx"
 import AdminUsersContainer from "./features/users/admin/admin.users.container.tsx"
 import "./index.css"
-import RankingBackofficeContainer from "./features/backoffice/ranking/ranking.backoffice.container.tsx";
+
 
 const rootElement = document.getElementById("root")
 
@@ -26,7 +26,6 @@ if (rootElement) {
           <Route path="bootcamp/:id" element={<ProfileBootcampContainer />} />
           <Route path="backoffice" element={<BackofficeContainer />} />
           <Route path="bootcamp/:id" element={<ProfileBootcampContainer />} />
-          <Route path="backoffice/ranking" element={<RankingBackofficeContainer />} />
         </Routes>
       </React.StrictMode>
     </BrowserRouter>
