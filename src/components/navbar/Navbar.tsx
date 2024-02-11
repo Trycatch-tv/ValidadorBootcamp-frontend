d7d866f6eba1ce574f258ce1b107a833fc29cd39
orliluq <orlibet@gmail.com>
2024-02-10 22:31:12 -0400

    corregido

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -99,25 +99,25 @@ const handleDropdownClick = () => {
             type="button" 
             onClick={handleDropdownClick}
           >
-            Backoffice 
+            Dashboard 
             <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
             </svg>
           </button>
 
-        <div id="dropdown" className={`absolute top-[50px] left-auto right-15 z-50 w-44 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 ${dropdownOpen ? '' : 'hidden'}`}>
-          <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+        <div id="dropdown" className={`absolute top-[50px] left-auto right-15 z-50 w-44 bg-blue divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 ${dropdownOpen ? '' : 'hidden'}`}>
+          <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton"> 
             <li>
-              <a href="/backoffice/ranking" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Ranking</a>
+              <a href="/backoffice" className="block px-4 py-2 hover:bg-gray-400 dark:hover:bg-gray-600 dark:hover:text-white">Backoffice</a>
             </li>
             <li>
-              <a href="/backoffice/bootcamp" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Bootcamps</a>
+              <a href="/bootcamp" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Bootcamps</a>
             </li>
             <li>
-              <a href="/backoffice/reports" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Reports</a>
+              <a href="/reports" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Reports</a>
             </li>
             <li>
-              <a href="/backoffice/users" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Users</a>
+              <a href="/users" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Users</a>
             </li>
           </ul>
         </div>
