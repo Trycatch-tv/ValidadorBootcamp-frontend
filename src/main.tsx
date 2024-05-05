590399e2994814db6acd2be4bd99374a964dc390
Jorge Villafraz <jorge9372@gmail.com>
2024-05-05 13:00:55 -0400

    feat: router updated to new syntax of react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,18 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import SigninContainer from "./features/auth/signin/signin.auth.container.tsx"
-import SignupContainer from "./features/auth/signup/signup.auth.container.tsx"
-import BackofficeContainer from "./features/backoffice/dashboard/dashboard.backoffice.container.tsx"
-import ProfileBootcampContainer from "./features/bootcamps/profile/profile.bootcamp.container.tsx"
-import HomeContainer from "./features/home/Home.container.tsx"
-import RankingContainer from "./features/ranking/Ranking.container.tsx"
-import AdminUsersContainer from "./features/users/admin/admin.users.container.tsx"
-import "./index.css"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const rootElement = document.getElementById("root")
+import { MyRoutes } from './router/index.tsx';
+
+import './index.css';
+
+const rootElement = document.getElementById('root');
 
 if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
-    <BrowserRouter>
-      <React.StrictMode>
-        <Routes>
-          <Route path="/" element={<HomeContainer />} />
-          <Route path="signin" element={<SigninContainer />} />
-          <Route path="signup" element={<SignupContainer />} />
-          <Route path="ranking" element={<RankingContainer />} />
-          <Route path="bootcamp/:id" element={<ProfileBootcampContainer />} />
-          <Route path="backoffice" element={<BackofficeContainer />} />
-          <Route path="backoffice/users" element={<AdminUsersContainer />} />
-        </Routes>
-      </React.StrictMode>
-    </BrowserRouter>
-  )
+    ReactDOM.createRoot(rootElement).render(
+        <React.StrictMode>
+            <MyRoutes />
+        </React.StrictMode>
+    );
 } else {
-  console.error("No se encontró el elemento con ID 'root'")
+    console.error("No se encontró el elemento con ID 'root'");
 }
