87f92c245fcc93037646a91c9d2e864c4e63e13c
JudLup Luna <judlup@gmail.com>
2024-09-11 22:00:03 -0500

    feat: agregar searchbar de bootcamps en el backoffice

--- src/features/backoffice/bootcamps/bootcamps.backoffice.view.tsx
+++ src/features/backoffice/bootcamps/bootcamps.backoffice.view.tsx
@@ -1,6 +1,6 @@
-import { Button, CardContent, CardHeader, Input } from "@/components/ui"
+import { CardContent, CardHeader } from "@/components/ui"
+import SearchBarContainer from "@/features/bootcamps/searchbar/searchbar.container"
 import Layout from "@/layout/Layout"
-import { Filter } from "lucide-react"
 import { FC } from "react"
 import ListBootcampsBackofficeContainer from "./list/list.bootcamps.backoffice.container"
 
@@ -16,22 +16,7 @@ const BootcampsBackofficeView: FC<Props> = ({ bootcamps }) => {
           <h5 className="font-semibold ">Directorio de Bootcamps</h5>
         </CardHeader>
         <CardContent className="w-fit">
-          <div className="flex gap-3 mb-16">
-            <Input
-              className="max-w-[440px]"
-              id="name"
-              placeholder="Buscar bootcamp... "
-            />
-            <div className="flex justify-content-between gap-2">
-              <Button size={"sm"} className="text-xs" variant="outline">
-                <Filter className="mr-2 h-4 w-4" />
-                Filtros
-              </Button>
-              <Button size={"sm"} className="text-xs">
-                Buscar
-              </Button>
-            </div>
-          </div>
+          <SearchBarContainer />
           <ListBootcampsBackofficeContainer bootcamps={bootcamps} />
         </CardContent>
       </Layout>
