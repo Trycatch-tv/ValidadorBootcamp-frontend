9cf06df356e798cea63e7c5ca8a7d411d7ec71be
JudLup Luna <judlup@gmail.com>
2024-10-22 21:27:57 -0500

    refactor: refactorizar editar bootcamp desde backoffice

--- src/features/backoffice/bootcamps/edit/edit.bootcamp.backoffice.container.tsx
+++ src/features/backoffice/bootcamps/edit/edit.bootcamp.backoffice.container.tsx
@@ -11,43 +11,49 @@ interface Props {
 }
 
 const EditBootcampBackofficeContainer: FC<Props> = ({ bootcamp }) => {
-  const [name, setName] = useState(bootcamp?.name)
-  const [description, setDescription] = useState(bootcamp?.description)
-  const [website, setWebsite] = useState(bootcamp?.url)
-  const [facebook, setFacebook] = useState(bootcamp?.facebook_url)
-  const [instagram, setInstagram] = useState(bootcamp?.instragram_url)
-  const [country, setCountry] = useState(bootcamp?.country_name)
-  const [isoCountry, setIsoCountry] = useState(bootcamp?.country_iso)
-  const [email, setEmail] = useState(bootcamp?.email)
-  const [phone, setPhone] = useState(bootcamp?.phone)
-  const [isEndorsed, setIsEndorsed] = useState(bootcamp?.is_endorsed)
-  const [endorsedBy, setEndorsedBy] = useState(bootcamp?.endorsed_by)
-  const [isVerified, setIsVerified] = useState(bootcamp?.is_verified)
-  const [avatar, setAvatar] = useState(bootcamp?.avatar)
-  const [selectedFile, setSelectedFile] = useState<File | null>(null)
-  const [mode, setMode] = useState(bootcamp?.mode)
-  const [enableEndorsedBy, setEnableEndorsedBy] = useState(false)
+  const [form, setForm] = useState({
+    name: bootcamp.name,
+    description: bootcamp.description,
+    website: bootcamp.url,
+    facebook: bootcamp.facebook_url,
+    instagram: bootcamp.instragram_url,
+    country: bootcamp.country_name,
+    isoCountry: bootcamp.country_iso,
+    email: bootcamp.email,
+    phone: bootcamp.phone,
+    isEndorsed: bootcamp.is_endorsed,
+    endorsedBy: bootcamp.endorsed_by,
+    isVerified: bootcamp.is_verified,
+    avatar: bootcamp.avatar,
+    mode: bootcamp.mode,
+    enableEndorsedBy: false,
+    selectedFile: undefined as File | undefined,
+  })
 
   const { getBootcamps } = useBootcampStore((state) => state)
   const { setModalState } = useGlobalStore((state) => state)
+
+  const handleUpdateForm = (changes: object) => {
+    setForm((currentState) => ({ ...currentState, ...changes }))
+  }
 
   const editBootcamp = async () => {
     const bootcampController = new BootcampController()
 
     const editBootcamp = {
-      name,
-      description,
-      url: website,
-      facebook_url: facebook,
-      instragram_url: instagram,
-      country_name: country,
-      country_iso: isoCountry,
-      email,
-      phone,
-      is_endorsed: isEndorsed,
-      endorsed_by: endorsedBy,
-      is_verified: isVerified,
-      mode,
+      name: form.name,
+      description: form.description,
+      url: form.website,
+      facebook_url: form.facebook,
+      instragram_url: form.instagram,
+      country_name: form.country,
+      country_iso: form.isoCountry,
+      email: form.email,
+      phone: form.phone,
+      is_endorsed: form.isEndorsed,
+      endorsed_by: form.endorsedBy,
+      is_verified: form.isVerified,
+      mode: form.mode,
     }
 
     const editBootcampResponse = await bootcampController.updateOne(
@@ -70,83 +76,13 @@ const EditBootcampBackofficeContainer: FC<Props> = ({ bootcamp }) => {
       )
     }
   }
-
-  // ----
-
-  // const [objeto, setObjeto] = useState({ firstName: "", lastName: "" })
-  // const handleUpdateObjeto = (changes) => {
-  //   setObjeto((currentState) => ({ ...currentState, ...changes }))
-  // }
-
-  // ;<input onChange={(e) => handleUpdateObjeto({ firstName: e.target.value })} />
-
-  // ----
-
-  const handleName = (value: string) => {
-    setName(value)
-  }
-
-  const handleDescription = (value: string) => {
-    setDescription(value)
-  }
-
-  const handleWebsite = (value: string) => {
-    setWebsite(value)
-  }
-
-  const handleFacebook = (value: string) => {
-    setFacebook(value)
-  }
-
-  const handleInstagram = (value: string) => {
-    setInstagram(value)
-  }
-
-  const handleCountry = (value: string) => {
-    setCountry(value)
-  }
-
-  const handleIsoCountry = (value: string) => {
-    setIsoCountry(value)
-  }
-
-  const handleEmail = (value: string) => {
-    setEmail(value)
-  }
-
-  const handlePhone = (value: string) => {
-    setPhone(value)
-  }
-
-  const handleIsEndorsed = (value: boolean) => {
-    setIsEndorsed(value)
-
-    if (value) {
-      setEnableEndorsedBy(false)
-    } else {
-      setEnableEndorsedBy(true)
-      setEndorsedBy("")
-    }
-  }
-
-  const handleEndorsedBy = (value: string) => {
-    setEndorsedBy(value)
-  }
-
-  const handleIsVerified = (value: boolean) => {
-    setIsVerified(value)
-  }
-
-  const handleMode = (value: string) => {
-    setMode(value)
-  }
-
   const handleAvatar = async (file: File) => {
     const bootcampController = new BootcampController()
-    if (file === null) {
+    if (file === null || file === undefined) {
       alert("No file selected")
     }
-    setSelectedFile(file)
+
+    setForm((currentState) => ({ ...currentState, selectedFile: file }))
     const uploadAvatarResponse = await bootcampController.uploadAvatar(
       bootcamp.id,
       file
@@ -157,7 +93,10 @@ const EditBootcampBackofficeContainer: FC<Props> = ({ bootcamp }) => {
         "The avatar was uploaded successfully",
         "success"
       )
-      setAvatar(uploadAvatarResponse.avatar)
+      setForm((currentState) => ({
+        ...currentState,
+        avatar: uploadAvatarResponse.avatar,
+      }))
       getBootcamps()
     }
   }
@@ -165,36 +104,11 @@ const EditBootcampBackofficeContainer: FC<Props> = ({ bootcamp }) => {
   return (
     <>
       <EditBootcampBackofficeView
-        handleName={handleName}
-        handleDescription={handleDescription}
-        handleWebsite={handleWebsite}
-        handleFacebook={handleFacebook}
-        handleInstagram={handleInstagram}
-        handleCountry={handleCountry}
-        handleIsoCountry={handleIsoCountry}
-        handleEmail={handleEmail}
-        handlePhone={handlePhone}
-        handleIsEndorsed={handleIsEndorsed}
-        handleEndorsedBy={handleEndorsedBy}
-        handleIsVerified={handleIsVerified}
         handleEditBootcamp={editBootcamp}
-        handleMode={handleMode}
-        isEndorsed={isEndorsed}
-        isVerified={isVerified}
-        endorsedBy={endorsedBy}
-        name={name}
-        description={description}
-        website={website}
-        facebook={facebook}
-        instagram={instagram}
-        country={country}
-        isoCountry={isoCountry}
-        email={email}
-        phone={phone}
-        mode={mode}
-        enableEndorsedBy={enableEndorsedBy}
         onAvatarChange={handleAvatar}
-        selectedFile={selectedFile}
+        selectedFile={form.selectedFile}
+        onUpdateForm={handleUpdateForm}
+        form={form}
       />
     </>
   )
